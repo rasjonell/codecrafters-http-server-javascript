64ac63acb0b6247c5a8ac6d5502ed2e7f3b374b5
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2024-01-07 19:00:26 +0400

    Respond with HTTP resposne

--- app/main.js
+++ app/main.js
@@ -1,13 +1,18 @@
-const net = require("net");
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const net = require('net');
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+  socket.on('close', () => {
+    console.log('[Socket] closed');
+
     socket.end();
     server.close();
   });
+
+  socket.on('data', (data) => {
+    console.log('[Socket] data received', data);
+    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    socket.end();
+  });
 });
 
-server.listen(4221, "localhost");
+server.listen(4221, 'localhost');
