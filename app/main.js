0f6f843b519b4c53dc466bd4b577e4efc60657fb
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2024-01-07 19:14:42 +0400

    Parse HTTP request path, method, version

--- app/main.js
+++ app/main.js
@@ -1,5 +1,7 @@
 const net = require('net');
 
+const CRLF = '\r\n\r\n';
+
 const server = net.createServer((socket) => {
   socket.on('close', () => {
     console.log('[Socket] closed');
@@ -9,10 +11,30 @@ const server = net.createServer((socket) => {
   });
 
   socket.on('data', (data) => {
-    console.log('[Socket] data received', data);
-    socket.write('HTTP/1.1 200 OK\r\n\r\n');
+    const parsedData = parseData(data);
+    console.log('[Socket] data received', parsedData);
+
+    socket.write(
+      parsedData.path === '/' ? `HTTP/1.1 200 OK${CRLF}` : `HTTP/1.1 404 Not Found${CRLF}`,
+    );
+
     socket.end();
   });
 });
+
+/**
+ * @param dataBuf {Buffer}
+ */
+function parseData(dataBuf) {
+  const data = dataBuf.toString();
+  const lines = data.split('\r\n');
+  const [method, path, version] = lines[0].split(' ');
+
+  return {
+    path,
+    method,
+    version,
+  };
+}
 
 server.listen(4221, 'localhost');
